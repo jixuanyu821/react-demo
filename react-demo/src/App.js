085f91a70b2da49fe74044ff8d2534c008b2a64b
jixuanyu <jixy@hzcominfo.com>
2019-11-22 10:15:24 +0800

    react 实践

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TimeMachine from './index.jsx'
+// function MessageBox(props){
+// return <h1>messageBox!{props.msg}</h1> 
+// }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TimeMachine/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="./index1.html"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          rel="noopener noreferrer">Learn React
         </a>
       </header>
     </div>
---
+++ react-demo/src/index.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+function FormatterData1(props){
+  return <h2>时间{props.date.toLocaleTimeString()}</h2>
+}
+class FormatterData extends React.Component {
+  render () {
+    return (
+      <div>
+        <h2>时间{this.props.date.toLocaleTimeString()}</h2>
+      </div>
+    )
+  }
+}
+class MessageBox extends React.Component{
+  render(){
+    return (
+    <h1>messageBox!{this.props.msg}</h1>
+    )
+  }
+}
+class TimeMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+    console.log(props);
+    this.alertFun = this.alertFun.bind(this);
+  }
+  alertFun(e){
+    e.preventDefault()//阻止默认事件
+    console.log(e)
+    console.log(e.target)//当前点击元素
+    console.log(this)
+  }
+  componentDidMount () {
+    this.timerId = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount () {
+    clearInterval(this.timerId)
+  }
+  tick () {
+    this.setState({
+      date: new Date()
+    })
+  }
+  
+  render () {
+    return (
+      <div>
+        <MessageBox msg="world" />
+        <FormatterData1 date={this.state.date}  msg="world" />
+        <FormatterData date={this.state.date}  msg="world" />
+        <a href="www.baidu.com" onClick={ (e) => this.alertFun(e)}>123</a>
+        <MessageBox1/>
+      </div>
+    );
+  }
+}
+class MessageBox1 extends React.Component{
+  alertFun(e){
+    e.preventDefault()
+    console.log(e)
+    console.log(e.target)
+    console.log(this)
+  }
+  render(){
+    return(
+      <a onClick={function(e) {console.log(e,'this')}} >234</a>
+    )
+  }
+}
+// function alertFun(data,e){
+//   console.log(e)
+//   // e.preventDefault();
+//     console.log(this)
+//     alert(data)
+// }
+export default TimeMachine
